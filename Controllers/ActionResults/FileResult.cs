d8eba6d9881b3c4f780b8f8e2fff181aa1c20ff9
Daniel <info@danielvaladas.com>
2022-02-10 07:57:40 -0500

    Created some demos

---
+++ Controllers/ActionResults/FileResult.cs
@@ -0,0 +1,52 @@
+﻿// MIT License
+// Copyright Eraware
+
+using DotNetNuke.Services.FileSystem;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Eraware.Modules.SummitApiDemo.Controllers.ActionResults
+{
+    /// <summary>
+    /// A result that returns a file as an attachment.
+    /// </summary>
+    public class FileResult : IHttpActionResult
+    {
+        private readonly IFileInfo fileInfo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileResult"/> class.
+        /// </summary>
+        /// <param name="fileInfo">The file to return (DNN file in this case).</param>
+        public FileResult(IFileInfo fileInfo)
+        {
+            this.fileInfo = fileInfo;
+        }
+
+        /// <inheritdoc/>
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return new Task<HttpResponseMessage>(
+                () =>
+                {
+                    var fileContent = FileManager.Instance.GetFileContent(this.fileInfo);
+                    var response = new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StreamContent(fileContent),
+                    };
+                    response.Content.Headers.ContentType = new MediaTypeHeaderValue(this.fileInfo.ContentType);
+                    response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+                    response.Content.Headers.ContentDisposition.FileName = this.fileInfo.FileName;
+                    response.Content.Headers.ContentType = new MediaTypeHeaderValue(this.fileInfo.ContentType.ToString());
+                    response.Content.Headers.ContentLength = fileContent.Length;
+
+                    return response;
+                },
+                cancellationToken);
+        }
+    }
+}
---
+++ Controllers/CacheWebApiAttribute.cs
@@ -0,0 +1,52 @@
+﻿// MIT License
+// Copyright Eraware
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Eraware.Modules.SummitApiDemo.Controllers
+{
+    /// <summary>
+    /// Caches web api results on the client side.
+    /// </summary>
+    public class CacheWebApiAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheWebApiAttribute"/> class.
+        /// </summary>
+        public CacheWebApiAttribute()
+        {
+            this.Duration = TimeSpan.FromMinutes(20);
+        }
+
+        /// <summary>
+        /// Gets or sets the time to cache on the client.
+        /// </summary>
+        public TimeSpan Duration { get; set; }
+
+        /// <inheritdoc/>
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            base.OnActionExecuted(actionExecutedContext);
+            actionExecutedContext.Response.Headers.CacheControl = new System.Net.Http.Headers.CacheControlHeaderValue()
+            {
+                MaxAge = this.Duration,
+                Private = true,
+            };
+        }
+
+        /// <inheritdoc/>
+        public override async Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            await base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+            actionExecutedContext.Response.Headers.CacheControl = new System.Net.Http.Headers.CacheControlHeaderValue()
+            {
+                MaxAge = this.Duration,
+                Private = true,
+            };
+        }
+    }
+}
